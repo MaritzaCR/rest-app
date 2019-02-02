b6b6124cfaa3e96262f635e201da1e497c91da34
MaritzaCR <maritza.cardenasr@gmail.com>
2019-02-02 11:55:08 -0300

    chore: user's scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#Borro al comienzo para que no me duplique la información.
+User.delete_all
+
+# Crearemos 5 usuarios
+
+5.times do |index|
+    #User.create!(name: "sergio", age:33)
+    #La siguente linea es interpolar (#), se pone ! para que si arroja error m lo muestre
+    User.create!(name: "user #{index}", age:33)
+
+end
